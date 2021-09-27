7c3401a0f3db1cccffe2b0055485d57f09c4b56b
Tahjae <tahjaecampbell15@gmail.com>
2021-09-27 10:42:03 -0500

    Nodemon & Concurrency Setup

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const products = require('./data/products');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('API is running...'));
+app.get('/', (req, res) => res.send('API is running....'));
 
 app.get('/api/products', (req, res) => {
 	res.json(products);
