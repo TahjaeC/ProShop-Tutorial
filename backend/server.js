626295d81a6a3d242110feac3f0a3ba84f17bf1c
Tahjae <tahjaecampbell15@gmail.com>
2021-09-30 10:49:31 -0500

    Custom Error Handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -14,6 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('API is running....'));
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(PORT, () =>
 	console.log(
