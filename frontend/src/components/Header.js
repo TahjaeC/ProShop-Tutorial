47192e13068c02100270f2774d355efc402ea977
Tahjae <tahjaecampbell15@gmail.com>
2021-09-30 20:08:05 -0500

    Product Details Reducer & Action

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,16 +13,21 @@ const Header = () => {
 
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='ml-auto'>
+						<Nav className='ms-auto'>
 							<LinkContainer to='/cart'>
 								<Nav.Link>
-									<i className='fas fa-shopping-cart'></i>Cart
+									<i
+										className='fas fa-shopping-cart'
+										style={{ 'margin-right': '5px' }}></i>
+									Cart
 								</Nav.Link>
 							</LinkContainer>
 
 							<LinkContainer to='/login'>
 								<Nav.Link>
-									<i className='fas fa-user'></i>
+									<i
+										className='fas fa-user'
+										style={{ 'margin-right': '5px' }}></i>
 									Sign In
 								</Nav.Link>
 							</LinkContainer>
